7cbf73ea9ca08503356f9b356a5fcbf41d3e9709
mauricechouam <lansichouamou@gmail.com>
2020-06-10 21:10:37 -0700

    added Employe.js source code

---
+++ Develop/lib/Employee.js
@@ -0,0 +1,18 @@
+class Employee {
+    constructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+    getName() {
+        return this.name;
+    }
+    getID() {
+        return this.id;
+    }
+    getEmail() {
+        return this.email
+    }
+
+}
+module.export = Employee;
