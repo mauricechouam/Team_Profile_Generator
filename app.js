5a2892c071cd5252aa4b273548e63ef8df4bb17a
mauricechouam <lansichouamou@gmail.com>
2020-06-12 22:14:50 -0700

    added oupt file test

--- app.js
+++ app.js
@@ -8,36 +8,60 @@ const inputQuestions = async (inputs = []) => {
     const prompts = [
         {
             name: "employeeName",
-            message: "please enter the employee name",
+            message: "please enter the employee name!",
             valide: valideName
         },
         {
             name: "employeeID",
-            message: "please enter the employee ID",
+            message: "please enter the employee ID!",
             valide: valideID
         },
         {
             name: "employeeEmail",
-            message: "please enter the employee Email",
+            message: "please enter the employee Email!",
             default: "employeea@mail.com",
             valide: valideEmail
         },
         {
             type: "list",
             name: "employeeRole",
-            message: "please select the Employee Role",
+            message: "please select the Employee Role!",
             choices: ["Manager", "Engineer", "Intern"]
         },
         {
             name: "officeNum",
-            message: "please enter the office number",
+            message: "please enter the office number!",
+            when: function (answer) {
+                return answer["employeeRole"] === "Manager"
+            }
+        },
+        {
+            name: "github",
+            message: "please enter the Employee Git hub!",
             when: function (answer) {
                 return answer["employeeRole"] === "Engineer"
-            }   
+            }
+        },
+        {
+            name: "schoolname",
+            message: "please enter the School Name",
+            when: function (answer) {
+                return answer["employeeRole"] === "Intern"
+            }
+        },
 
+        {
+            type: "confirm",
+            name: "more",
+            message: "would you like to add more employeee?"
         }
 
-
     ]
+
+
+    const { more, ...answer } = await inquirer.prompt(prompts);
+    const newInputs = [...inputs, answers];
+    return more ? inputQuestions(newInputs) : newInputs;
 }
 
+const r
